a1ac9e875b2a7b4e544006051fe28484f337fa51
josue rojas <josuerojas.rojas@gmail.com>
2020-03-28 17:37:15 -0500

    update rules; created functions for delete and create new post

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const db = admin.database();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addPost = functions.database
+  .ref("/post")
+  .onCreate((snapshot, context) => {
+    db.ref("/amountPost")
+      .once("value")
+      .then(_snapshot =>
+        db.ref("/amountPost").set(_snapshot.val() ? _snapshot.val() + 1 : 1)
+      )
+      .catch(e => db.ref("/amountPost").set(1));
+  });
+
+exports.deletePost = functions.database
+  .ref("/post")
+  .onDelete((snapshot, context) => {
+    db.ref("/amountPost")
+      .once("value")
+      .then(_snapshot =>
+        db
+          .ref("/amountPost")
+          .set(_snapshot.val() && _snapshot.val() > 0 ? _snapshot.val() - 1 : 0)
+      )
+      .catch(e => db.ref("/amountPost").set(0));
+  });
